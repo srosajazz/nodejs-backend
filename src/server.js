d373214b2232bd4a9ceaabbf4d216862fc33f042
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-09 20:30:57 -0400

    Back-end development

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on("connection", socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  })
+
+});
+
+mongoose.connect('mongodb+srv://serge:serge@cluster0-tgr6q.mongodb.net/serge?retryWrites=true', {
+  useNewUrlParser: true
+});
+
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+
+app.use(express.json()); //registra utilizacao de json
+app.use(express.urlencoded({ // autoriza o envio de arquivos
+  extended: true
+}));
+app.use(require("./routes")); //importa nossas rotas
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
+
+server.listen(3333);
